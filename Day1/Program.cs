133df26df105734366040a83ecb40cd08a1e3355
Boostio <70571116+BoostioAaron@users.noreply.github.com>
2022-12-09 18:48:38 +0100

    Day 1: Part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -4,8 +4,6 @@ var path = Path.Combine(slice.ToArray());
 IDictionary<int, int> elves = new Dictionary<int, int>();
 var count = 0;
 var totalCalories = 0;
-var highest = 0;
-var highestElf = 0;
 
 foreach (var line in File.ReadLines(path + @"./Calories.txt"))
     if (line.Length == 0)
@@ -20,13 +18,10 @@ foreach (var line in File.ReadLines(path + @"./Calories.txt"))
         totalCalories += calories;
     }
 
-foreach (var (key, currentNumber) in elves)
-{
-    if (currentNumber > highest)
-    {
-        highest = currentNumber;
-        highestElf = key;
-    }
-}
+var first = elves.Values.Max();
+var second = elves.Values.Where(x => x < first).Max();
+var third = elves.Values.Where(x => x < second).Max();
+var answer = first + second + third;
 
-Console.WriteLine("{0} Has the the most calories with {1} calories", highestElf, highest);
+
+Console.WriteLine("Calories combined: " + answer);

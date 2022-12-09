c9211035efb2091316ab4efc2378fe1f3aa9a49f
Boostio <70571116+BoostioAaron@users.noreply.github.com>
2022-12-10 00:28:13 +0100

    DAY 2: PART 2

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -4,16 +4,18 @@ var path = Path.Combine(slice.ToArray());
 
 IDictionary<string, int> Outcomes = new Dictionary<string, int>
 {
-    { "A X", 4 },
-    { "A Y", 8 },
-    { "A Z", 3 },
+    { "A X", 3 },
+    { "A Y", 4 },
+    { "A Z", 8 },
     { "B X", 1 },
     { "B Y", 5 },
     { "B Z", 9 },
-    { "C X", 7 },
-    { "C Y", 2 },
-    { "C Z", 6 }
+    { "C X", 2 },
+    { "C Y", 6 },
+    { "C Z", 7 }
 };
+
+
 var lines = File.ReadAllLines(path + "./Puzzle.txt");
 var sum = lines.Select(round => Outcomes[round]).Sum();
 Console.WriteLine(sum);
